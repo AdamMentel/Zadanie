034bbeecae6ec65dcb3a756621d8d884aa5d12d3
AdamMentel <adamko.mentel@gmail.com>
2024-06-05 22:49:36 +0200

    novadb

--- connect.php
+++ connect.php
@@ -2,13 +2,12 @@
 $servername = "localhost";
 $username = "userdb";
 $password = "databaza";
-$dbname = "northwindmysql";
+$dbname = "northwind";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
